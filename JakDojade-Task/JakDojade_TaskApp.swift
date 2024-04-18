57a2d1850cb1b024ad5633902ccaa68f4bece968
Jakub Sedal <sedal.jakub@gmail.com>
2024-04-18 12:54:44 +0200

    Initial Commit

---
+++ JakDojade-Task/JakDojade_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  JakDojade_TaskApp.swift
+//  JakDojade-Task
+//
+//  Created by Jakub Sędal on 18/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct JakDojade_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
